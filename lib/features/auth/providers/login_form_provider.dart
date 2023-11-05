85e0d0121402f72123889cde48fe988699fb50d8
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2023-11-05 08:33:59 +0100

    Se conecta el formulario de login con su provider

--- lib/features/auth/providers/login_form_provider.dart
+++ lib/features/auth/providers/login_form_provider.dart
@@ -1,6 +1,55 @@
-//! 1 - State del provider
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:formz/formz.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
+//! 3 - StateNotifierProvider - consume afuera
+final loginFormProvider =
+    StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
+  return LoginFormNotifier();
+});
+
+//! 2 - Como implementamos un notifier
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
+  LoginFormNotifier() : super(LoginFormState());
+
+  onEmailChange(String email) {
+    final newEmail = Email.dirty(email);
+    state = state.copyWith(
+      email: newEmail,
+      isValid: Formz.validate([newEmail, state.password]),
+    );
+  }
+
+  onPasswordChange(String password) {
+    final newPassword = Password.dirty(password);
+    state = state.copyWith(
+      password: newPassword,
+      isValid: Formz.validate([state.email, newPassword]),
+    );
+  }
+
+  onFormSubmit() {
+    _touchEveryField();
+
+    if (!state.isValid) return;
+
+    print(state);
+  }
+
+  _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+
+    state = state.copyWith(
+      isFormPosted: true,
+      email: email,
+      password: password,
+      isValid: Formz.validate([email, password]),
+    );
+  }
+}
+
+//! 1 - State del provider
 class LoginFormState {
   final bool isPosting;
   final bool isFormPosted;
@@ -15,8 +64,31 @@ class LoginFormState {
     this.email = const Email.pure(),
     this.password = const Password.pure(),
   });
+
+  LoginFormState copyWith({
+    bool? isPosting,
+    bool? isFormPosted,
+    bool? isValid,
+    Email? email,
+    Password? password,
+  }) =>
+      LoginFormState(
+        isPosting: isPosting ?? this.isPosting,
+        isFormPosted: isFormPosted ?? this.isFormPosted,
+        isValid: isValid ?? this.isValid,
+        email: email ?? this.email,
+        password: password ?? this.password,
+      );
+
+  @override
+  String toString() {
+    return '''
+      Login FormState:
+        isPosting: $isPosting
+        isFormPosted: $isFormPosted
+        isValid: $isValid
+        email: $email
+        password: $password
+    ''';
+  }
 }
-
-//! 2 - Como implementamos un notifier
-
-//! 3 - StateNotifierProvider - consume afuera
